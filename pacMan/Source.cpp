bb87e66a0672fee5ee84f46109fdfa03c7106818
AhmedAlaa248 <ahmed.elrweney@yahoo.com>
2023-04-08 23:47:10 +0200

    cleaning code

--- pacMan/Source.cpp
+++ pacMan/Source.cpp
@@ -1,13 +1,22 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
-#include "Animation.h"
 using namespace sf;
 using namespace std;
 
+int xPosition = 0;
+int yPositon = 0;
 const int arrSize = 19;
 
-void drawMaze(int arr[arrSize][arrSize], Sprite maze[arrSize][arrSize], Texture& wall, Texture& coin) {
+struct images
+{
+    Texture texture;
+    Sprite sprite;
+};
+
+images Images[6];
+
+void drawMaze(Sprite maze[arrSize][arrSize], Texture& wall, Texture& coin) {
 
     //0 wall
     //1 coin
@@ -22,8 +31,8 @@ void drawMaze(int arr[arrSize][arrSize], Sprite maze[arrSize][arrSize], Texture&
          {0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0},//7
          {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0},//8
          {0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0},//9
-         {0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0},//10
-         {1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},//11
+         {0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 0, 0, 1, 0, 1, 0, 0, 0, 0},//10
+         {1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1},//11
          {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},//12
          {0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0},//13
          {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},//14
@@ -38,12 +47,12 @@ void drawMaze(int arr[arrSize][arrSize], Sprite maze[arrSize][arrSize], Texture&
         for (int j = 0; j < arrSize; j++) {
             if (board[i][j] == 0){
                 maze[i][j].setTexture(wall);
-                maze[i][j].setPosition(480+j*50,50+ i*50);
+                maze[i][j].setPosition(480+j*50 ,45.5+ i*50);
                 maze[i][j].setScale(3.4, 3.4);
             }
             else if (board[i][j] == 1) {
                 maze[i][j].setTexture(coin);
-                maze[i][j].setPosition(480+j*50.5,50+ i* 50.5);
+                maze[i][j].setPosition(480+j*50.5, 45.5 + i* 50.5);
             }
             
         }
@@ -51,87 +60,109 @@ void drawMaze(int arr[arrSize][arrSize], Sprite maze[arrSize][arrSize], Texture&
 
 }
 
-class Movement
-{
-public:
-    Movement() {};
-
-    void movementDirection(const int& dir, sf::Sprite& spritesheet)
+void movingPacman(Sprite& pacMan , int& x , int& y) {
+    if (Keyboard::isKeyPressed(Keyboard::Left) && pacMan.getPosition().x > 0)
     {
-        switch (dir) {
-        case 0:
-            spritesheet.move(sf::Vector2f(0, -.5));
-            break;
-        case 1:
-            spritesheet.move(sf::Vector2f(0, .5));
-            break;
-        case 2:
-            spritesheet.move(sf::Vector2f(.5, 0));
-            break;
-        case 3:
-            spritesheet.move(sf::Vector2f(-.5, 0));
-            break;
-        }
+        pacMan.move(-0.5, 0);
+        x++;
+        x %= 3;
+        y = 1;
+        pacMan.setTextureRect(IntRect(x * 16, y * 16, 16, 16));
+
     }
-};
+    else if (Keyboard::isKeyPressed(Keyboard::Right) && pacMan.getPosition().x < 1864)
+    {
+        pacMan.move(0.5, 0);
+        x++;
+        x %= 3;
+        y = 0;
+        pacMan.setTextureRect(IntRect(x * 16, y * 15, 16, 15));
+
+    }
+    else if (Keyboard::isKeyPressed(Keyboard::Up) && pacMan.getPosition().y > 0)
+    {
+        pacMan.move(0, -0.5);
+        x++;
+        x %= 3;
+        y = 2;
+        pacMan.setTextureRect(IntRect(x * 15.5, y * 16, 15.5, 16));
+
+    }
+    else if (Keyboard::isKeyPressed(Keyboard::Down) && pacMan.getPosition().y < 1024)
+    {
+        pacMan.move(0, 0.5);
+        x++;
+        x %= 3;
+        y = 3;
+        pacMan.setTextureRect(IntRect(x * 16, y * 16, 16, 16));
+
+    }
+
+}
+
+Sprite pacMandrawing(Texture& texture) {
+    
+    Sprite pacManSprite;
+    pacManSprite.setTexture(texture);
+    pacManSprite.setTextureRect(IntRect(xPosition * 17, yPositon * 15, 17, 15));
+    pacManSprite.setPosition(960, 500);
+    pacManSprite.setScale(3, 3);
+
+    return pacManSprite;
+}
+
+Sprite ghostsDrawing(Texture& texture) {
+
+    Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setTextureRect(IntRect(xPosition * 16, yPositon * 16, 16, 16));
+    sprite.setScale(2.5, 2.5);
+
+    return sprite;
+}
 
 int main()
 {
-    int x = 0;
-    int y = 0;
-
     // Create the window
-    RenderWindow window(sf::VideoMode(1920, 1080), "Pac-Man");
-    Vector2i source(DIRECTION_X_DOWN_INITAL, DIRECTION_Y_DOWN_INITAL);
-    Movement movement;
+    RenderWindow window(VideoMode(1920, 1080), "Pac-Man");
     
-
-    int boar[arrSize][arrSize];
+    int board[arrSize][arrSize];
+    
+    // Intializing textures
     Texture wall, coin;
+
+    // Load the images
+    Images[0].texture.loadFromFile("Textures/pacmansprite.png");
+    Images[1].texture.loadFromFile("Textures/redGhost.png");
+    Images[2].texture.loadFromFile("Textures/pinkGhost.png");
+    Images[3].texture.loadFromFile("Textures/cyanGhost.png");
+    Images[4].texture.loadFromFile("Textures/orangeGhost.png");
+    
     wall.loadFromFile("Textures/Wall.png");
     coin.loadFromFile("Textures/coin.png");
 
+    // Intializing sprites
     Sprite maze[arrSize][arrSize];
+    Images[0].sprite = pacMandrawing(Images[0].texture);
+    Images[1].sprite = ghostsDrawing(Images[1].texture);
+    Images[2].sprite = ghostsDrawing(Images[2].texture);
+    Images[3].sprite = ghostsDrawing(Images[3].texture);
+    Images[4].sprite = ghostsDrawing(Images[4].texture);
 
-    drawMaze(boar, maze, wall, coin);
+    Images[2].sprite.setPosition(50, 50);
+    Images[3].sprite.setPosition(50, 300);
+    Images[4].sprite.setPosition(50, 200);
 
-    // Set the background color
-    window.clear(Color::Black);
-
-    // Load the images
-    Texture pacManTexture;
-    pacManTexture.loadFromFile("Textures/pacmansprite.png");
-
-    // Create the Pac-Man sprite
-    Sprite pacMan;
-    pacMan.setTexture(pacManTexture);
-    
-    pacMan.setTextureRect(IntRect(x*17, y *15 ,17, 15));
-    pacMan.setPosition(960, 500);
-    pacMan.setScale(3, 3);
-    sf::Clock clock;
-    sf::Time prevTime = clock.getElapsedTime();
-
-   
-   
-  /*  Animation animation(&pacManTexture, Vector2u(3, 4), 0.3f);
-    float deltaTime = 0.0f;
-    sf::Clock clock;*/
-    //pacMan.setPosition(1920/2, 1080/2);
-
+    drawMaze(maze, wall, coin);
 
    // Main loop
     while (window.isOpen())
     {
-        //deltaTime = clock.restart().asSeconds();
-
         // Handle events
         Event event;
         while (window.pollEvent(event))
           
         {
-            sf::Time elapsedTime = clock.getElapsedTime() - prevTime;
-            prevTime = prevTime + elapsedTime;
             // Close the window if the close button is pressed
             if (event.type == Event::Closed)
             {
@@ -139,58 +170,19 @@ int main()
             }
         }
 
-        // Move Pac-Man
+
+        movingPacman(Images[0].sprite, xPosition , yPositon);
         
-        if (Keyboard::isKeyPressed(Keyboard::Left)&& pacMan.getPosition().x>0)
-        {
-            movement.movementDirection(3, pacMan);
-            source.x = DIRECTION_X_LEFT_INITAL;
-            source.y = DIRETION_Y_LEFT_INITAL;
-            x++;
-            x %= 3;
-            y =1 ;
-            pacMan.setTextureRect(IntRect(x * 16, y * 16, 16, 16));
-                
-           }
-        else if (Keyboard::isKeyPressed(Keyboard::Right)&& pacMan.getPosition().x < 1864)
-        {
-            movement.movementDirection(2, pacMan);
-            source.x = DIRECTION_X_RIGHT_INITAL;
-            source.y = DIRECTION_Y_RIGHT_INITAL;
-            x++;
-            x %= 3;
-            y = 0;
-            pacMan.setTextureRect(IntRect(x * 16, y*15 ,16, 15));
-            
-        }
-        else if (Keyboard::isKeyPressed(Keyboard::Up)&&pacMan.getPosition().y >0)
-        {
-            movement.movementDirection(0, pacMan);
-            source.x = DIRECTION_X_UP_INITAL;
-            source.y = DIRECTION_Y_UP_INITAL;
-          
-            x++;
-            x %= 3;
-            y = 2;
-            pacMan.setTextureRect(IntRect(x * 15.5, y * 16, 15.5, 16));
-            
-        }
-        else if (Keyboard::isKeyPressed(Keyboard::Down)&& pacMan.getPosition().y<1024)
-        {
-            movement.movementDirection(1, pacMan);
-            source.x = DIRECTION_X_DOWN_INITAL;
-            source.y = DIRECTION_Y_DOWN_INITAL;
-            x++;
-            x %= 3;
-            y = 3;
-            pacMan.setTextureRect(IntRect(x * 16, y * 16, 16, 16));
-            
-        }
-        //animation.updateImage(0, deltaTime, true);
-        //  pacMan.setTextureRect(animation.uvRect);
-        // Draw the Pac-Man sprite
+        //Draw the Pac-Man sprite
+        
         window.clear(Color::Black);
-        window.draw(pacMan);
+        window.draw(Images[0].sprite);
+        window.draw(Images[1].sprite);
+        window.draw(Images[2].sprite);
+        window.draw(Images[3].sprite);
+        window.draw(Images[4].sprite);
+        
+        // Draw the maze
         for (int i = 0 ; i < arrSize; i++)
         {
             for (int j = 0; j < arrSize; j++) {
@@ -199,6 +191,5 @@ int main()
         }
         window.display();
     }
-
     return 0;
 }
