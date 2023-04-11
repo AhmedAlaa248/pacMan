a1ee7403470cff959167b8c41f33f31d0c98504b
AhmedAlaa248 <ahmed.elrweney@yahoo.com>
2023-04-11 09:18:55 +0200

    Maze edited

--- pacMan/Source.cpp
+++ pacMan/Source.cpp
@@ -1,12 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+
 using namespace sf;
 using namespace std;
 
 int xPosition = 0;
 int yPositon = 0;
-const int arrSize = 19;
+int xStartPosition[8];
+int yStartPosition[8];
+
+const int rows = 23;
+const int columns = 21;
 
 struct images
 {
@@ -14,47 +19,94 @@ struct images
     Sprite sprite;
 };
 
-images Images[6];
+images Images[8];
+// 0 wall, 1 coin, 2 redGhost, 3 pinkGhost, 4 cyanGhost, 5 orangeGhost, 6 pacMan 
 
-void drawMaze(Sprite maze[arrSize][arrSize], Texture& wall, Texture& coin) {
+void drawMaze(Sprite maze[rows][columns]) {
+    Images[0].texture.loadFromFile("Textures/Wall.png");
+    Images[1].texture.loadFromFile("Textures/coin.png");
+    
+    // 0 wall
+    // 1 coin
+    // 2 redGhostsPlace
+    // 3 pinkGhostsPlace
+    // 4 cyanGhostsPlace
+    // 5 orangeGhostsPlace
+    // 6 pacManPlace
+    // 9 empty cell
 
-    //0 wall
-    //1 coin
-    int board[arrSize][arrSize] = {
-       // 1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8  9
-         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//1
-         {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},//2
-         {0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0},//3
-         {0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0},//4
-         {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//5
-         {0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},//6
-         {0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0},//7
-         {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0},//8
-         {0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0},//9
-         {0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 0, 0, 1, 0, 1, 0, 0, 0, 0},//10
-         {1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1},//11
-         {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},//12
-         {0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0},//13
-         {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},//14
-         {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},//15
-         {0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0},//16
-         {0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0},//17
-         {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//18
-         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//20
+    int board[rows][columns] = {
+       // 0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8  9  0  
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//0
+         {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//1
+         {0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},//2
+         {0, 1, 0, 9, 0, 1, 0, 9, 0, 1, 0, 1, 0, 9, 0, 1, 0, 9, 0, 1, 0},//3
+         {0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},//4
+         {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//5
+         {0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0},//6
+         {0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0},//7
+         {0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0},//8
+         {0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0},//9
+         {0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 2, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0},//10
+         {1, 1, 1, 1, 1, 1, 1, 1, 0, 4, 5, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1},//11
+         {0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0},//12
+         {0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 6, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0},//13
+         {0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0},//14
+         {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//15
+         {0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},//16
+         {0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0},//17
+         {0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0},//18
+         {0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0},//19
+         {0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0},//20
+         {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},//21
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//22
     };
 
-    for (int i = 0; i < arrSize; i++) {
-        for (int j = 0; j < arrSize; j++) {
-            if (board[i][j] == 0){
-                maze[i][j].setTexture(wall);
-                maze[i][j].setPosition(480+j*50 ,45.5+ i*50);
-                maze[i][j].setScale(3.4, 3.4);
-            }
-            else if (board[i][j] == 1) {
-                maze[i][j].setTexture(coin);
-                maze[i][j].setPosition(480+j*50.5, 45.5 + i* 50.5);
-            }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+
+            switch (board[i][j])
+            {
+                case 0:
+                    maze[i][j].setTexture(Images[0].texture);
+                    maze[i][j].setPosition(480 + j * 45, 10 + i * 45);
+                    maze[i][j].setScale(2.9, 2.9);
+                    break;
+
+                case 1:
+                    maze[i][j].setTexture(Images[1].texture);
+                    maze[i][j].setPosition(480 + j * 45, 10 + i * 45);
+                    break;
             
+                // setting initial pos for monsters
+                case 2:
+                    yStartPosition[2] = 10 + i *45;
+                    xStartPosition[2] = 480 + j *45;
+                    break;
+
+                case 3:
+                    yStartPosition[3] = 10 + i * 45;
+                    xStartPosition[3] = 480 + j * 45;
+                    break;
+
+                case 4:
+                    yStartPosition[4] = 10 + i * 45;
+                    xStartPosition[4] = 480 + j * 45;
+                    break;
+
+                case 5:
+                    yStartPosition[5] = 10 + i * 45;
+                    xStartPosition[5] = 480 + j * 45;
+                    break;
+
+                // setting initial pos for pacMan
+                case 6:
+                    yStartPosition[6] = 10 + i * 45;
+                    xStartPosition[6] = 480 + j * 45;
+            default:
+                break;
+            }
+        
         }
     }
 
@@ -100,68 +152,51 @@ void movingPacman(Sprite& pacMan , int& x , int& y) {
 
 }
 
-Sprite pacMandrawing(Texture& texture) {
-    
-    Sprite pacManSprite;
-    pacManSprite.setTexture(texture);
-    pacManSprite.setTextureRect(IntRect(xPosition * 17, yPositon * 15, 17, 15));
-    pacManSprite.setPosition(960, 500);
-    pacManSprite.setScale(3, 3);
+void pacManDrawing() {
+    Images[6].texture.loadFromFile("Textures/pacmansprite.png");
 
-    return pacManSprite;
+    Images[6].sprite.setTexture(Images[6].texture);
+    Images[6].sprite.setTextureRect(IntRect(xPosition * 17, yPositon * 15, 17, 15));
+    Images[6].sprite.setPosition(xStartPosition[6], yStartPosition[6]);
+    Images[6].sprite.setScale(3, 3);
+
 }
 
-Sprite ghostsDrawing(Texture& texture) {
+void ghostsDrawing() {
+    
+    Images[2].texture.loadFromFile("Textures/redGhost.png");
+    Images[3].texture.loadFromFile("Textures/pinkGhost.png");
+    Images[4].texture.loadFromFile("Textures/cyanGhost.png");
+    Images[5].texture.loadFromFile("Textures/orangeGhost.png");
 
     Sprite sprite;
-    sprite.setTexture(texture);
-    sprite.setTextureRect(IntRect(xPosition * 16, yPositon * 16, 16, 16));
-    sprite.setScale(2.5, 2.5);
+    for (int i = 2; i < 6; i++)
+    {
+        Images[i].sprite.setTexture(Images[i].texture);
+        Images[i].sprite.setTextureRect(IntRect(xPosition * 16, yPositon * 16, 16, 16));
+        Images[i].sprite.setScale(2.5, 2.5);
+        Images[i].sprite.setPosition(xStartPosition[i], yStartPosition[i]);
 
-    return sprite;
+    }
 }
 
 int main()
 {
     // Create the window
     RenderWindow window(VideoMode(1920, 1080), "Pac-Man");
+   
+    Sprite maze[rows][columns];
     
-    int board[arrSize][arrSize];
-    
-    // Intializing textures
-    Texture wall, coin;
+    drawMaze(maze);
+    pacManDrawing();
+    ghostsDrawing();
 
-    // Load the images
-    Images[0].texture.loadFromFile("Textures/pacmansprite.png");
-    Images[1].texture.loadFromFile("Textures/redGhost.png");
-    Images[2].texture.loadFromFile("Textures/pinkGhost.png");
-    Images[3].texture.loadFromFile("Textures/cyanGhost.png");
-    Images[4].texture.loadFromFile("Textures/orangeGhost.png");
-    
-    wall.loadFromFile("Textures/Wall.png");
-    coin.loadFromFile("Textures/coin.png");
-
-    // Intializing sprites
-    Sprite maze[arrSize][arrSize];
-    Images[0].sprite = pacMandrawing(Images[0].texture);
-    Images[1].sprite = ghostsDrawing(Images[1].texture);
-    Images[2].sprite = ghostsDrawing(Images[2].texture);
-    Images[3].sprite = ghostsDrawing(Images[3].texture);
-    Images[4].sprite = ghostsDrawing(Images[4].texture);
-
-    Images[2].sprite.setPosition(50, 50);
-    Images[3].sprite.setPosition(50, 300);
-    Images[4].sprite.setPosition(50, 200);
-
-    drawMaze(maze, wall, coin);
-
-   // Main loop
+    // Main loop
     while (window.isOpen())
     {
         // Handle events
         Event event;
         while (window.pollEvent(event))
-          
         {
             // Close the window if the close button is pressed
             if (event.type == Event::Closed)
@@ -170,22 +205,18 @@ int main()
             }
         }
 
+        movingPacman(Images[6].sprite, xPosition , yPositon);
+        
+        //Drawing sprites
 
-        movingPacman(Images[0].sprite, xPosition , yPositon);
-        
-        //Draw the Pac-Man sprite
-        
         window.clear(Color::Black);
-        window.draw(Images[0].sprite);
-        window.draw(Images[1].sprite);
-        window.draw(Images[2].sprite);
-        window.draw(Images[3].sprite);
-        window.draw(Images[4].sprite);
-        
+        for (int i = 2; i < 7; i++) {
+            window.draw(Images[i].sprite);
+        }
+
         // Draw the maze
-        for (int i = 0 ; i < arrSize; i++)
-        {
-            for (int j = 0; j < arrSize; j++) {
+        for (int i = 0 ; i < rows; i++){
+            for (int j = 0; j < columns; j++){
                 window.draw(maze[i][j]);
             }
         }
